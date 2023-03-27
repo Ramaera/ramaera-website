2a5b5afa3ff1b7e82fe2563ffd695a481e5f0c4d
Owais-Ramaera <owais.it@ramaera.in>
2023-03-27 11:01:45 +0530

    Job Create Form Value Submitted

--- state/store/index.js
+++ state/store/index.js
@@ -4,6 +4,7 @@ import logInUserReducer from "../slice/userSlice"
 import projectReducer from "../slice/projectSlice"
 import applicationDataReducer from "../slice/applicantDataSlice"
 import accessTokenReducer from "../slice/accessTokenSlice"
+import jobReducer from "../slice/jobSlice"
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +13,6 @@ export const store = configureStore({
     project: projectReducer,
     applicationData: applicationDataReducer,
     accessToken: accessTokenReducer,
+    jobApplication:jobReducer,
   },
 })
