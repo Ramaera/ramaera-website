1f1a6af78fcaca9f77158f56328144074a1730e1
rajnishramaera <rajnish.it@ramaera.in>
2023-07-25 14:07:30 +0530

    image update

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
       <Leader />
       {/* <Trust /> */}
       <Promoting />
-      <Tomorrow />
+      {/* <Tomorrow /> */}
       {/* <Faqs /> */}
       <Transforming />
       <Footer />
