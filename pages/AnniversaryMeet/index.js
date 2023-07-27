8b5dcc00c970acc3266a1b23f79aff1a7a7cb4ec
Owais <owais.it@ramaera.in>
2023-07-27 17:50:58 +0530

    Anniversary Card

--- pages/AnniversaryMeet/index.js
+++ pages/AnniversaryMeet/index.js
@@ -1,30 +1,159 @@
-import Navbar from "../../components/Navbar/Navbar"
-import PageLayout from "../../components/PageLayout/PageLayout"
-import PageWidth from "../../components/Width/PageWidth"
-import Footer from "../page/common/footer"
-import VisitLocations from "../page/annualMeeting/VisitLocations"
-import styled from "styled-components"
+import Navbar from "../../components/Navbar/Navbar";
+import PageLayout from "../../components/PageLayout/PageLayout";
+import PageWidth from "../../components/Width/PageWidth";
+import Footer from "../page/common/footer";
+import VisitLocations from "../page/annualMeeting/VisitLocations";
+import styled from "styled-components";
+import Text from "@/components/Text/Text";
+import Button from "@/components/Button/Button";
+import { useState } from "react";
+import SouthIcon from "@mui/icons-material/South";
 
 const HeightDiv = styled.div`
   height: 150px;
   @media only screen and (max-width: 510px) {
     height: 100px;
   }
-`
+`;
+
+const CardDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  align-items: center;
+  @media only screen and (max-width: 510px) {
+    flex-direction: column;
+  }
+`;
+
+const ImgDiv = styled.div`
+  width: 50%;
+  padding: 5%;
+  @media only screen and (max-width: 510px) {
+    width: 80%;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  gap: 0.5rem;
+  justify-content: flex-end;
+  margin-bottom: 1rem;
+  @media (max-width: 450px) {
+    justify-content: center;
+  }
+`;
+
 const JobPost = () => {
+  const [show, setShow] = useState(false);
   return (
     <>
       <PageLayout mheight="100vh" height="120vh">
         <Navbar />
         <HeightDiv />
-        <PageWidth full scale={"95%"} width="1500px" position="relative">
-          <VisitLocations />
-        </PageWidth>
+        <div
+          style={{
+            marginTop: "2%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+          <div>
+            <div data-aos="zoom-in-up" data-aos-duration="800">
+              <Text
+                Text="You are heartly invited to"
+                lg="linear-gradient(to right, #ffa73d, gold)"
+                font
+                size="clamp(2rem, 1.5vw, 2rem)"
+                fw="400"
+                align="center"
+                lh="50px"
+                m="0 0 1rem 0"
+                xmsize="clamp(2.4rem, 1.5vw, 2rem)"
+                xssize="clamp(2.4rem, 1.5vw, 2rem)"
+                msize="1.1rem"
+                mwidth="100%"
+                mmwidth="100%"
+                padding="0"
+                mpadding="0"
+                mta="center"
+                mlh="unset"
+              />
+              <Text
+                Text="Ramaera Grand Anniversary"
+                lg="linear-gradient(to right, #ffa73d, gold)"
+                font
+                size="clamp(2rem, 1.5vw, 2rem)"
+                fw="400"
+                align="center"
+                lh="50px"
+                m="0 0 1rem 0"
+                xmsize="clamp(2.4rem, 1.5vw, 2rem)"
+                xssize="clamp(2.4rem, 1.5vw, 2rem)"
+                msize="1.1rem"
+                mwidth="100%"
+                mmwidth="100%"
+                padding="0"
+                mpadding="0"
+                mta="center"
+                mlh="unset"
+              />
+            </div>
+            <CardDiv>
+              <ImgDiv data-aos="slide-right" data-aos-duration="1200">
+                <img
+                  src="/content/anniversary.jpg"
+                  alt="anniversary card"
+                  style={{ width: "100%" }}
+                />
+              </ImgDiv>
+
+              <ImgDiv data-aos="slide-left" data-aos-duration="1200">
+                <img
+                  src="/content/anniversaryBack.jpg"
+                  alt="anniversary card"
+                  style={{ width: "100%" }}
+                />
+              </ImgDiv>
+            </CardDiv>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+            }}>
+            <h3>Click here to register for the 1st anniversary meeting.</h3>
+            <SouthIcon style={{ fontSize: "50px", color: "orange" }} />
+          </div>
+          <a href="#register">
+            <ButtonContainer onClick={() => setShow(true)}>
+              <button
+                style={{ background: "none", border: "none" }}
+                type="submit">
+                <Button
+                  nav
+                  width="140px"
+                  height="2.75rem"
+                  Text="Register"
+                  inheight="2.5rem"
+                />
+              </button>
+            </ButtonContainer>
+          </a>
+        </div>
+        <div id="register">
+          <PageWidth full scale={"95%"} width="1500px" position="relative">
+            {show && <VisitLocations />}
+          </PageWidth>
+        </div>
         <hr style={{ borderBottom: "1px solid white", width: "80vw" }} />
         <Footer />
       </PageLayout>
     </>
-  )
-}
+  );
+};
 
-export default JobPost
+export default JobPost;
--- pages/page/annualMeeting/VisitLocations.js
+++ pages/page/annualMeeting/VisitLocations.js
@@ -1,20 +1,20 @@
-import { VisitForm } from "./VisitForm"
-import Button from "../../../components/Button/SubmitButton"
-import { VISIT_US_ANNUAL } from "../../../apollo/queries/visitUs"
-import { useMutation } from "@apollo/client"
-import { useSelector } from "react-redux"
-import { ToastContainer, toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { Container, ButtonContainer } from "./allJobStyle.js"
-import { useState, useEffect } from "react"
-import styled from "styled-components"
+import { VisitForm } from "./VisitForm";
+import Button from "../../../components/Button/SubmitButton";
+import { VISIT_US_ANNUAL } from "../../../apollo/queries/visitUs";
+import { useMutation } from "@apollo/client";
+import { useSelector } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Container, ButtonContainer } from "./allJobStyle.js";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const Br = styled.br`
   display: none;
   @media only screen and (max-width: 510px) {
     display: flex;
   }
-`
+`;
 const CounterDiv = styled.div`
   font-size: 1.6rem;
   padding: 8rem;
@@ -23,7 +23,7 @@ const CounterDiv = styled.div`
     font-size: 1.2rem;
     padding: 2rem;
   }
-`
+`;
 const CounterDivMobile = styled.div`
   text-align: center;
   font-size: 1.4rem;
@@ -32,46 +32,46 @@ const CounterDivMobile = styled.div`
   @media only screen and (max-width: 510px) {
     display: flex;
   }
-`
+`;
 const Countdown = () => {
   const [countdownDate, setCountdownDate] = useState(
     new Date("05/04/2023").getTime()
-  )
+  );
   const [state, setState] = useState({
     days: 0,
     hours: 0,
     minutes: 0,
     seconds: 0,
-  })
+  });
 
   useEffect(() => {
-    setInterval(() => setNewTime(), 1000)
-  }, [])
+    setInterval(() => setNewTime(), 1000);
+  }, []);
 
   const setNewTime = () => {
     if (countdownDate) {
-      const currentTime = new Date().getTime()
+      const currentTime = new Date().getTime();
 
-      const distanceToDate = countdownDate - currentTime
+      const distanceToDate = countdownDate - currentTime;
 
-      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24))
+      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
       let hours = Math.floor(
         (distanceToDate % (1000 * 60 * 60 * 24 * 12)) / (1000 * 60 * 60) + 12
-      )
+      );
       let minutes = Math.floor(
         (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
-      )
-      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000)
+      );
+      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
 
-      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-      days = `${days}`
-      setState({ days: days, hours: hours, minutes, seconds })
+      days = `${days}`;
+      setState({ days: days, hours: hours, minutes, seconds });
     }
-  }
-  state.hours >= 0 ? state.hours : (state.hours = 0)
-  state.minutes >= 0 ? state.minutes : (state.minutes = 0)
-  state.seconds >= 0 ? state.seconds : (state.seconds = 0)
+  };
+  state.hours >= 0 ? state.hours : (state.hours = 0);
+  state.minutes >= 0 ? state.minutes : (state.minutes = 0);
+  state.seconds >= 0 ? state.seconds : (state.seconds = 0);
 
   return (
     <div>
@@ -81,31 +81,31 @@ const Countdown = () => {
         {state.seconds || "00"} Seconds
       </div>
     </div>
-  )
-}
+  );
+};
 
 const VisitLocations = () => {
-  const [clickOnce, setClickOnce] = useState(0)
+  const [clickOnce, setClickOnce] = useState(0);
 
-  const [createGeneralMeetingVisitorForm] = useMutation(VISIT_US_ANNUAL)
-  const fromDateVar = useSelector((state) => state.visitingInfo.fromDate)
-  const toDateVar = useSelector((state) => state.visitingInfo.toDate)
-  const emailVar = useSelector((state) => state.visitingInfo.email)
+  const [createGeneralMeetingVisitorForm] = useMutation(VISIT_US_ANNUAL);
+  const fromDateVar = useSelector((state) => state.visitingInfo.fromDate);
+  const toDateVar = useSelector((state) => state.visitingInfo.toDate);
+  const emailVar = useSelector((state) => state.visitingInfo.email);
   const mobileNumberVar = useSelector(
     (state) => state.visitingInfo.mobileNumber
-  )
-  const nameVar = useSelector((state) => state.visitingInfo.name)
+  );
+  const nameVar = useSelector((state) => state.visitingInfo.name);
   const numberOfPeopleVar = useSelector(
     (state) => state.visitingInfo.numberOfPeople
-  )
-  const plantNameVar = useSelector((state) => state.visitingInfo.plantName)
-  const pwIdVar = useSelector((state) => state.visitingInfo.pwId)
-  const reasonVar = useSelector((state) => state.visitingInfo.reason)
-  const typeOfVisitVar = useSelector((state) => state.visitingInfo.typeOfVisit)
-  const addressVar = useSelector((state) => state.visitingInfo.address)
-  const theDate = fromDateVar + " " + toDateVar
+  );
+  const plantNameVar = useSelector((state) => state.visitingInfo.plantName);
+  const pwIdVar = useSelector((state) => state.visitingInfo.pwId);
+  const reasonVar = useSelector((state) => state.visitingInfo.reason);
+  const typeOfVisitVar = useSelector((state) => state.visitingInfo.typeOfVisit);
+  const addressVar = useSelector((state) => state.visitingInfo.address);
+  const theDate = fromDateVar + " " + toDateVar;
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!pwIdVar) {
       toast.error("Your PW ID is not Approved or invalid PW ID", {
         position: "top-center",
@@ -118,7 +118,7 @@ const VisitLocations = () => {
         draggable: true,
         progress: undefined,
         theme: "dark",
-      })
+      });
     }
     if (clickOnce === 0 && pwIdVar) {
       try {
@@ -135,7 +135,7 @@ const VisitLocations = () => {
             typeOfVisit: typeOfVisitVar,
             address: addressVar,
           },
-        })
+        });
         toast.success(
           `Your Response has been submitted, Welcome to the Anniversary Meet`,
           {
@@ -150,7 +150,7 @@ const VisitLocations = () => {
             progress: undefined,
             theme: "light",
           }
-        )
+        );
         //setClickOnce(2)
         //clearForm()
       } catch (error) {
@@ -165,15 +165,15 @@ const VisitLocations = () => {
           draggable: true,
           progress: undefined,
           theme: "light",
-        })
+        });
       }
     }
-  }
+  };
   const clearForm = () => {
     setTimeout(() => {
-      location.reload()
-    }, "4200")
-  }
+      location.reload();
+    }, "4200");
+  };
 
   return (
     <>
@@ -190,13 +190,12 @@ const VisitLocations = () => {
             Registration has been closed for Annual General Meet
           </div>
         </CounterDiv> */}
-         <div
+        <div
           style={{
             display: "flex ",
             justifyContent: "center",
             alignItems: "center",
-          }}
-        ></div>
+          }}></div>
         <form onSubmit={(e) => handleSubmit(e)}>
           <ToastContainer
             toastClassName="toast-font"
@@ -223,14 +222,12 @@ const VisitLocations = () => {
               top: ".5rem",
               right: ".5rem",
               fontSize: ".8rem",
-            }}
-          ></div>
+            }}></div>
           <VisitForm />
           <ButtonContainer>
             <button
               style={{ background: "none", border: "none" }}
-              type="submit"
-            >
+              type="submit">
               <Button
                 nav
                 width="140px"
@@ -243,7 +240,7 @@ const VisitLocations = () => {
         </form>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default VisitLocations
+export default VisitLocations;
