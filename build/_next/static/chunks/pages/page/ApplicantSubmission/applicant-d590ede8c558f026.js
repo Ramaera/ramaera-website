9ade68613f7cf4ae692a9c596915198b6100ac41
Owais <owais.it@ramaera.in>
2023-07-28 16:56:09 +0530

    Build add more name

---
+++ build/_next/static/chunks/pages/page/ApplicantSubmission/applicant-d590ede8c558f026.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6863],{32149:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/page/ApplicantSubmission/applicant",function(){return u(66083)}])}},function(n){n.O(0,[1502,3838,1664,2249,8964,2482,2613,989,6083,9774,567,179],function(){return n(n.s=32149)}),_N_E=n.O()}]);
---
+++ build/_next/static/chunks/pages/page/ProjectApplicationDetail/ApplicationDetail-42dda9ea21396cea.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6894],{85185:function(n,i,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/page/ProjectApplicationDetail/ApplicationDetail",function(){return _(11173)}])}},function(n){n.O(0,[1502,1664,1173,9774,567,179],function(){return n(n.s=85185)}),_N_E=n.O()}]);
